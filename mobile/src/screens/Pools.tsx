dd9744d40bd52c806fa2d594af7666f7246dda92
jorgeeder <jorgeeder.js@gmail.com>
2023-01-30 16:28:02 -0300

    feat(mobile): shows loading icon or list of user pools

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { VStack, Icon } from "native-base";
+import { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
@@ -7,17 +7,35 @@ import { api } from "../services/api";
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
-import { PoolCard } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardPros } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardPros[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
 
   async function fetchPools() {
     try {
-      const response = await api.get("/pools")
-      console.log(response.data.pools)
-    } catch (error) {
+      setIsLoading(true);
 
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: "Não foi possível carregar os bolões",
+        placement: "top",
+        bgColor: "red.500"
+      });
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -36,6 +54,18 @@ export function Pools() {
           onPress={() => navigate("find")}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> :
+        <FlatList
+        data={pools}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <PoolCard data={item} />}
+        ListEmptyComponent={EmptyPoolList}
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{ pb: 10 }}
+        px={5}
+      />}
     </VStack>
   )
 }
