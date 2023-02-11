cc0462d1774cbaf50c5634b13290286778cbcd83
jorgeeder <jorgeeder.js@gmail.com>
2023-02-11 13:49:00 -0300

    feat(mobile): reloading list when focus returns to interface

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { api } from "../services/api";
 
@@ -39,9 +40,9 @@ export function Pools() {
     }
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchPools();
-  }, []);
+  }, []));
 
   return (
     <VStack flex={1} bgColor="gray.900">
